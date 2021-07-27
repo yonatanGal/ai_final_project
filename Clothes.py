a2c5492d6872634a91032d18f4f05dc380e62380
liorbenyaakov <lior.benyaakov@mail.huji.ac.il>
2021-07-27 12:33:26 +0300

    das

---
+++ Clothes.py
@@ -0,0 +1,5 @@
+
+colors = {"black", "white", "grey", "blue","yellow","red","green","orange","brown","pink","purple"}
+
+
+db = {"my_black_T_shirt":[3,18-30,"black"],"button long shirt":[5,5-20,"green"],"blouse":[7,15-35],"sport_tshirt":[1,15-40,"blue"]}
