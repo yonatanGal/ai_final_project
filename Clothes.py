1d7be3e21bc1c565aca994166f1a2aff2f2858b6
liorbenyaakov <lior.benyaakov@mail.huji.ac.il>
2021-07-27 12:45:55 +0300

    lkjl

--- Clothes.py
+++ Clothes.py
@@ -2,4 +2,5 @@
 colors = {"black", "white", "grey", "blue","yellow","red","green","orange","brown","pink","purple"}
 
 
-db = {"my_black_T_shirt":[3,18-30,"black"],"button long shirt":[5,5-20,"green"],"blouse":[7,15-35],"sport_tshirt":[1,15-40,"blue"]}
+db = {"my_black_T_shirt":[3,(18,30),"black"],"button long shirt":[5,(5,20),"green"],"blouse":[7,(15,35)],
+      "sport_tshirt":[1,(15,40),"blue"], "pyjama":[0,(12-22),"pink"]}
